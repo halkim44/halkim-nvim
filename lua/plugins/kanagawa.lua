bee28c63befd1ffb61461837b3e8ebae5b59be05
halkimWSL <halkimrajan@gmail.com>
2026-02-06 11:32:53 +0800

    Initial nvim config

---
+++ lua/plugins/kanagawa.lua
@@ -0,0 +1,13 @@
+return {
+  "rebelot/kanagawa.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("kanagawa").setup({
+      transparent = false,
+      theme = "wave",
+    })
+
+    vim.cmd("colorscheme kanagawa")
+  end,
+}
