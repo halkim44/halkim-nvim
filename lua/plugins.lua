913703c48fbf6c0d96074b654144ff6b35096caa
halkimWSL <halkimrajan@gmail.com>
2026-02-19 12:06:41 +0800

    added nvim-tree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,4 +2,5 @@ return {
   "folke/neodev.nvim",
   "folke/which-key.nvim",
   { "folke/neoconf.nvim", cmd = "Neoconf" },
+  { "nvim-tree/nvim-web-devicons", opts = {} },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,5 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
-  build = ":TSUpdate",
-
-  opts = {
-    indent = { enable = true },
-    highlight = { enable = true },
-    folds = { enable = true },
-    ensure_installed = {
-      "javascript",
-      "typescript",
-      "go",
-      "ruby",
-      "lua"
-    }
-  }
+  build = ":TSUpdate"
 }
